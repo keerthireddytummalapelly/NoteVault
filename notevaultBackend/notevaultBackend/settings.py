2c2a1c088b67b2c1b8d9a7593e3e4dfc2c374856
keerthireddytummalapelly <t.y.reddy19@gmail.com>
2024-10-04 18:52:10 -0400

    ReadMe file update

--- notevaultBackend/notevaultBackend/settings.py
+++ notevaultBackend/notevaultBackend/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +85,7 @@ DATABASES = {
         'ENGINE': 'djongo',
         'NAME': 'NoteVault',
         'CLIENT': {
-            'host': 'mongodb+srv://tummalapellykeerthireddy:qpOmuFfKE8RJDTmq@cluster0.u32wx.mongodb.net/NoteVault?retryWrites=true&w=majority',
+            'host': os.getenv('DATABASE_URL'),
             'ssl': True,  # Enable SSL if needed
         }
     }
